cddfdc58cdb4af1c4ac2a0c81785cd53e7b76bf6
Patton Parham <patton.parham@verizon.net>
2020-01-29 02:41:52 -0600

    Progress made in getting inquirer propmts up and running. More work to be done with getting data from SQL to view, add to or update

--- employeeTracker.js
+++ employeeTracker.js
@@ -10,8 +10,75 @@ password: "",
 database: "employee_trackerDB"
 });
 
-function runSearch(){
+connection.connect(function(err){
+    if (err) throw err;
+    runTracker();
+});
+
+function runTracker(){
 inquirer
-    .prompt({})
+    .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "Add departments, roles or employees",
+            "View departments, roles or employees",
+            "Update employee roles"
+        ]
+    })
+    .then(function(answer){
+        switch(answer.action){
+            case "Add departments, roles or employees":
+            addDRE();
+            break;
+
+            case "View departments, roles or employees":
+            viewDRE();
+            break;
+
+            case "Update employee roles":
+            updateEmployeeRole();
+            break;
+        }
+    });
+}
+
+function viewDRE(){
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "Which would you like to view information about?",
+            choices: [
+                "Departments",
+                "Roles",
+                "Employees"
+            ]
+
+
+        }).then(function(answer){
+            switch (answer.action){
+                
+                case "Departments":
+                viewDepartments();
+                break;
+
+                case "Roles":
+                viewRoles();
+                break;
+
+                case "Employees":
+                viewEmployees();
+                break;
+            }
+        });
+
 
 }
+
+// function viewDepartments(){
+//     let query = "SELECT * FROM departments WHERE ?";
+//     connection.query(query, {department: answer.departments})
+// }
+
